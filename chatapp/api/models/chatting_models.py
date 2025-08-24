c20a61396ea0b19123b9386514d4cf29c1b390d7
Tehman700 <tehmanfast@gmail.com>
2025-08-24 22:44:53 +0500

    This is for the issue related to Online Users in Dashboard. This issue is resolved (#6)

--- chatapp/api/models/chatting_models.py
+++ chatapp/api/models/chatting_models.py
@@ -9,6 +9,7 @@ class ChatGroup(models.Model):
         return self.group_name
 
 
+
 class GroupMessage(models.Model):
     group =models.ForeignKey(ChatGroup, related_name = 'chat_messages',on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
